7de93c93e7cfa8fbdcfb9440706418689420a2b0
PJ Frias <peejfrias@gmail.com>
2018-01-09 02:10:42 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,12 +2,15 @@ class Anagram
 
   attr_accessor :word
 
+  @anagrams = []
+
   def initialize(word)
     @word = word
   end
 
   def match(phrase)
-    phrase.split(" ").each do |word|
-      
+    phrase.split(" ").each_with_index do |word,index|
+      if word.sort == @word.sort
+
 
 end
